9e95733badc44fd13a7a84f2461bd088f30d5cbc
moez chroufa <chroufa.moezkhan@gmail.com>
2025-10-28 15:56:11 +0100

    fixing the auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,33 @@
-const User = require("../models/user");
-
+const USERDB = require("../models/user");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 module.exports = async (req, res, next) => {
-  const userID = req.headers["x-user-id"];
-  if (!userID) {
-    return res.status(401).json({ error: " no user id provided" });
-  }
+  try {
+    const header = req.headers["authorization"];
+    if (!header) {
+      return res.status(401).json({ error: "no token provided" });
+    }
+    const token = header.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ error: "token invalid format" });
+    }
 
-  const user = await User.findById(userID);
-  if (!user) {
-    return res.status(401).json({ error: " invalid user" });
-  }
+    // expected payload on success
+    // verify the payload format {id,username,email, role};
+    // id : ****._id;
 
-  req.user = user;
-  next();
+    const decodedToken = jwt.verify(token, process.env.SEC_KEY);
+
+    const fuser = await USERDB.findById(decodedToken.id);
+    if (!fuser) {
+      return res.status(401).json({ error: "user not found" });
+    }
+    req.user = fuser;
+    next();
+  } catch (error) {
+    console.error("error message : ", error.message);
+    return res
+      .status(500)
+      .json({ message: "internal server error", error: error.message });
+  }
 };
